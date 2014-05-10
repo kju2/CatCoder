4e22a25506a1e952f45b21667b71fb81a6415d56
kju2 <kju2@edlinger.me>
2014-05-10 07:38:06 +0200

    level 2

--- Q4U/main.py
+++ Q4U/main.py
@@ -1,37 +1,38 @@
 from __future__ import print_function
 
-def caluclate_initial_fund(n, start, end, funds):
+def calculate_max_initial_fund(n, funds):
     """
-    >>> caluclate_initial_fund(3, 2, 3, [20, 30, 10])
-    40
-    >>> caluclate_initial_fund(4, 2, 3, [40, -10, 20, 30])
-    10
-    >>> caluclate_initial_fund(*parse_input("input.1"))
-    -96
-    >>> caluclate_initial_fund(*parse_input("input.2"))
-    -949
-    >>> caluclate_initial_fund(*parse_input("input.3"))
-    -39
-    >>> caluclate_initial_fund(*parse_input("input.4"))
-    -248
-    >>> caluclate_initial_fund(*parse_input("input.5"))
-    -333
-    >>> caluclate_initial_fund(*parse_input("input.6"))
-    -267
+    >>> calculate_max_initial_fund(10, [2, 3, 4, -12, 3, 2, -4, 5, 6, -1])
+    12
+    >>> calculate_max_initial_fund(*parse_input("input.1"))
+    580
+    >>> calculate_max_initial_fund(*parse_input("input.2"))
+    722
+    >>> calculate_max_initial_fund(*parse_input("input.3"))
+    2566
+    >>> calculate_max_initial_fund(*parse_input("input.4"))
+    3207
+    >>> calculate_max_initial_fund(*parse_input("input.5"))
+    7332
+    >>> calculate_max_initial_fund(*parse_input("input.6"))
+    5541
     """
     """
     """
-    return sum(funds[start - 1 : end])
+    max_initial_fund = 0
+    for i in range(len(funds) - 1):
+        for j in range(i, len(funds) + 1):
+            if max_initial_fund < sum(funds[i:j]):
+                max_initial_fund = max(max_initial_fund, sum(funds[i:j]))
+    return max_initial_fund
 
 def parse_input(file_name):
     with open(file_name) as f:
         lines = f.read().splitlines()
     line = map(int, lines[0].split(' '))
     n = line.pop(0)
-    start = line.pop(0)
-    end = line.pop(0)
     funds = line[:]
-    return n, start, end, funds
+    return n, funds
 
 if __name__ == "__main__":
     import doctest
