32b790862f95a522850761c7a540cc7729e26d34
kju2 <kju2@edlinger.me>
2014-05-10 08:08:27 +0200

    level 4

--- Q4U/main.py
+++ Q4U/main.py
@@ -3,38 +3,25 @@ from __future__ import print_function
 def calculate_max_initial_fund(n, funds):
     """
     >>> print(calculate_max_initial_fund(10, [-3, 5, 2, -1, 3, -10, 4, 5, -2, -1]))
-    9 2 2 5 7 8
+    9
     >>> print(calculate_max_initial_fund(*parse_input("input.1")))
-    180 2 3 3 8 8
+    26314
     >>> print(calculate_max_initial_fund(*parse_input("input.2")))
-    175 2 2 3 7 7
+    226843
     >>> print(calculate_max_initial_fund(*parse_input("input.3")))
-    605 2 9 18 22 29
+    9024192
     >>> print(calculate_max_initial_fund(*parse_input("input.4")))
-    1902 2 63 80 123 187
+    24023857
     >>> print(calculate_max_initial_fund(*parse_input("input.5")))
-    1359 2 47 70 90 149
+    25019396
     >>> print(calculate_max_initial_fund(*parse_input("input.6")))
-    1556 2 30 68 129 147
+    24840859
     """
-    """
-    """
-    max_initial_fund = 0
-    possibilities = []
-
-    for i in range(len(funds) - 1):
-        for j in range(i, len(funds) + 1):
-            sum_funds = sum(funds[i:j])
-            if sum_funds == max_initial_fund:
-                possibilities.append((i, j))
-            if sum_funds > max_initial_fund:
-                possibilities = [(i, j)]
-                max_initial_fund = sum_funds
-    output = str(max_initial_fund) + ' ' + str(len(possibilities))
-    for (i, j) in possibilities:
-        output += ' ' + str(i + 1) + ' ' + str(j)
-
-    return output
+    max_ending_here = max_so_far = 0
+    for x in funds:
+        max_ending_here = max(0, max_ending_here + x)
+        max_so_far = max(max_so_far, max_ending_here)
+    return max_so_far
 
 def parse_input(file_name):
     with open(file_name) as f:
